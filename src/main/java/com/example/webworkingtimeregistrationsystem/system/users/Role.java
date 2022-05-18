d57694f26182c075a33fa9e7a1b760614009dc15
Dominik Stachowiak <57596761+domstach00@users.noreply.github.com>
2022-05-18 17:55:53 +0200

    Change file structure

---
+++ src/main/java/com/example/webworkingtimeregistrationsystem/system/users/Role.java
@@ -0,0 +1,11 @@
+package com.example.webworkingtimeregistrationsystem.system.users;
+
+public class Role {
+
+	private com.example.webworkingtimeregistrationsystem.system.enums.RoleType RoleType;
+
+	public void ChangeRole() {
+
+	}
+
+}
