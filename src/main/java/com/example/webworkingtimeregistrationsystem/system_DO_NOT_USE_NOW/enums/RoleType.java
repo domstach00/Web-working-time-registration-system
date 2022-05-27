1b70eac290b3d6b7e4cad7323d1217785179f298
Dominik Stachowiak <57596761+domstach00@users.noreply.github.com>
2022-05-27 15:43:28 +0200

    Remove useless files

--- src/main/java/com/example/webworkingtimeregistrationsystem/system_DO_NOT_USE_NOW/enums/RoleType.java
+++
@@ -1,7 +0,0 @@
-package com.example.webworkingtimeregistrationsystem.system_DO_NOT_USE_NOW.enums;
-
-public enum RoleType {
-	RegularUser,
-	Admin,
-	ProjectManager
-}
--- src/main/java/com/example/webworkingtimeregistrationsystem/system_DO_NOT_USE_NOW/events/DayOff.java
+++
@@ -1,9 +0,0 @@
-package com.example.webworkingtimeregistrationsystem.system_DO_NOT_USE_NOW.events;
-
-import com.example.webworkingtimeregistrationsystem.system_DO_NOT_USE_NOW.enums.DayOffType;
-
-public class DayOff extends Event {
-
-	private DayOffType Type;
-
-}
