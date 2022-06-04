a5171c24a5e4484e000028a9f022446238287eef
Dominik Stachowiak <57596761+domstach00@users.noreply.github.com>
2022-06-04 19:01:17 +0200

    Removed comments

--- src/main/java/com/example/webworkingtimeregistrationsystem/WebWorkingTimeRegistrationSystemApplication.java
+++ src/main/java/com/example/webworkingtimeregistrationsystem/WebWorkingTimeRegistrationSystemApplication.java
@@ -12,26 +12,5 @@ public class WebWorkingTimeRegistrationSystemApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(WebWorkingTimeRegistrationSystemApplication.class, args);
-
-
-//        ProjectGroup projectGroup = context.getBean(ProjectGroup.class);
-//
-//        User user = context.getBean(User.class);
-
-
-//        String url = "jdbc:sqlite:src/database/project.db";
-//        try {
-//            Connection connection = DriverManager.getConnection(url);
-//            Statement statement = connection.createStatement();
-//
-//            ResultSet result = statement.executeQuery("SELECT * FROM User");
-//
-//            while (result.next()){
-//                System.out.println(result.getString("Email"));
-//            }
-//
-//        } catch (SQLException e){
-//            e.printStackTrace();
-//        }
     }
 }
--- src/main/java/com/example/webworkingtimeregistrationsystem/WebWorkingTimeRegistrationSystemController.java
+++ src/main/java/com/example/webworkingtimeregistrationsystem/WebWorkingTimeRegistrationSystemController.java
@@ -1,6 +1,5 @@
 package com.example.webworkingtimeregistrationsystem;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
